012073c4283bf22c6fe4f984cf70e7ca99313df9
Jonathan Chen <jc1738593939@gmail.com>
2019-04-08 18:53:04 -0400

    more conversion

---
+++ views/BIP_login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bio Internship Portal</title>
+		<link rel="stylesheet" href="login.css"/>
+	</head>
+	<body style="background-color: #CFB53B; text-align: center;">
+		<h1>Biology Internship Portal</h1>
+		<h3>I am a:</h3>
+		<a href="student_jobs_page.php" class="loginButton" style="height:40px; width:90px">Student</a>
+		<a href="employer_jobs.php" class="loginButton" style="height:40px; width:90px">Employer</a>
+		<a href="user_list.php" class="loginButton" style="height:40px; width:90px">Admin</a>
+	</body>
+</html>
