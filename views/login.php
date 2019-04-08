a7e6a4bec073aab7a0a0c00baa3327c08809b43b
Woradorn K <chateau86@gmail.com>
2019-04-08 14:31:24 -0400

    Fake login nsystem implemented. Signed-off-by: Woradorn K <chateau86@gmail.com>

---
+++ views/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Biology Internship Portal</title>
+		<link rel="stylesheet" href="users2.css"/>
+	</head>
+	<body>
+        <form method="post" action="login_backend.php">
+          <select name="username">
+            <option value="Sengoku">Sengoku</option>
+            <option value="second">Second</option>
+            <option value="third">Third</option>
+          </select>
+          <input type="submit" value="Submit the form"/>
+        </form>
+    </body>
+</html>
---
+++ views/login_check.php
@@ -0,0 +1,6 @@
+<?php
+    print_r($_COOKIE);
+    if (array_key_exists('username', $_COOKIE)) {
+        echo sprintf("Logged-in user: %s",(string)$_COOKIE['username']);
+    }
+?>
