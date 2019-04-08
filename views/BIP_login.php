54461350878c071e6676c0842faf1040d91fb3a9
Woradorn K <chateau86@gmail.com>
2019-04-08 18:57:31 -0400

    Fixed BIP_login Signed-off-by: Woradorn K <chateau86@gmail.com>

--- views/BIP_login.php
+++ views/BIP_login.php
@@ -7,8 +7,8 @@
 	<body style="background-color: #CFB53B; text-align: center;">
 		<h1>Biology Internship Portal</h1>
 		<h3>I am a:</h3>
-		<a href="student_jobs_page.php" class="loginButton" style="height:40px; width:90px">Student</a>
-		<a href="employer_jobs.php" class="loginButton" style="height:40px; width:90px">Employer</a>
-		<a href="user_list.php" class="loginButton" style="height:40px; width:90px">Admin</a>
+        <a href="login.php" class="loginButton" style="height:40px; width:90px">Student</a>
+        <a href="login.php" class="loginButton" style="height:40px; width:90px">Employer</a>
+        <a href="login.php" class="loginButton" style="height:40px; width:90px">Admin</a>
 	</body>
 </html>
