2ea94bb11caa0d83e574d9c62163db4d24a72096
Woradorn K <chateau86@gmail.com>
2019-04-08 16:09:28 -0400

    Improved get_resume Signed-off-by: Woradorn K <chateau86@gmail.com>

--- views/get_resume.php
+++ views/get_resume.php
@@ -19,10 +19,13 @@
     //TODO Use stored procedure instead
     $sql = "SELECT resume_file, proper_file_name FROM resume_file_table where resume_file_id = ".$id;
     $retval = mysqli_query( $conn, $sql);
-    $row = mysqli_fetch_assoc($retval);
-    $pdf_data = $row['resume_file'];
-    //echo $info['mime'];
-    header("Content-type: application/pdf");
-    header("Content-Disposition: inline; filename=".$row['proper_file_name'].'"');
-    echo $pdf_data;
+    if($row = mysqli_fetch_assoc($retval)){
+        $pdf_data = $row['resume_file'];
+        //echo $info['mime'];
+        header("Content-type: application/pdf");
+        header("Content-Disposition: inline; filename=".$row['proper_file_name'].'"');
+        echo $pdf_data;
+    } else {
+        echo "Not found";
+    }
 ?>
